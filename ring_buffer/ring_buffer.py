c7b240008185f57c8f1e4a85dd2c9c5a06940eef
Michelle Scott <scottmm374@gmail.com>
2020-08-31 13:15:01 -0400

    trying different with deque

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,44 +4,58 @@ class RingBuffer:
         self.data = []
         self.tail = 0
         self.head = 0
-        self.size = 0
+        # self.size = 0
 
     def append(self, item):
-        if self.size < self.capacity:
-            self.data.append(item)
-            self.tail = (self.tail + 1) % self.capacity
-            self.size = self.size + 1
+        if self.size() == self.capacity-1:
+            self.dequeue()
 
-            self.head = (self.head + 1) % self.capacity
+        self.data.append(item)
+        self.tail = (self.tail + 1) % self.capacity
+        # self.size = self.size + 1
+        return True
 
-        else:
-            self.head = (self.head + 1) % self.capacity
-            self.data.append(item)
-            self.tail = (self.tail + 1) % self.capacity
+    def dequeue(self):
+        if self.size() == 0:
+            return ("Queue Empty!")
+        data = self.data[self.head]
+        # self.size = self.size - 1
+        self.head = (self.head + 1) % self.capacity
+        return data
+
+    # Calculating the size of the queue
+
+    def size(self):
+        if self.tail >= self.head:
+            return (self.tail-self.head)
+        return (self.capacity - (self.head-self.tail))
 
     def get(self):
-        if self.size == 0:
+        if self.size() == 0:
             print("Queue is empty")
             return None
         else:
             curr = self.head
 
-            for curr in range(self.size):
+            for curr in range(self.size()):
                 print("Value", self.data[curr])
                 curr = (curr + 1) % self.capacity
                 print(curr)
 
 
-buffer = RingBuffer(5)
-buffer.get()
+buffer = RingBuffer(3)
+# buffer.get()
 
 buffer.append('a')
 buffer.append('b')
 buffer.append('c')
+print("max")
+buffer.get()
 buffer.append('d')
 buffer.append('e')
-print("at max")
+print("replace two")
 buffer.get()
 buffer.append("f")
-print("after max")
+buffer.append("g")
+# print("after max")
 buffer.get()
