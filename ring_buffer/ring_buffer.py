9965dbccda8412d6f8a2c6152a6080d4c76a0696
Michelle Scott <scottmm374@gmail.com>
2020-08-31 15:18:17 -0400

    all tests pass

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,59 +2,27 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.data = []
-        # self.tail = 0
         self.head = 0
-        self.size = 0
 
     def append(self, item):
-        if self.size == self.capacity:
-            data = self.data[self.head]
-            self.data.remove(data)
+        # if Que is full
+        if len(self.data) == self.capacity:
+            # item equal to current head
+            self.data[self.head] = item
+        # Increment head to next index (New oldest Item)
             self.head = (self.head + 1) % self.capacity
-            self.size = self.size - 1
-            # return data
-            return self.append(item)
+
+        # If Que is not full Append new Item
         else:
             self.data.append(item)
-            self.size = self.size + 1
-
-    # def append(self, item):
-    #     if self.size() == self.capacity-1:
-    #         return None
-    #     self.data.append(item)
-    #     self.tail = (self.tail + 1) % self.capacity
-    #     # self.size = self.size + 1
-    #     return True
-
-    # def dequeue(self):
-    #     if self.size() == 0:
-    #         return ("Queue Empty!")
-    #     data = self.data[self.head]
-    #     # self.size = self.size - 1
-    #     self.head = (self.head + 1) % self.capacity
-    #     return data
-
-    # Calculating the size of the queue
-
-    # def size(self):
-    #     if self.tail >= self.head:
-    #         return (self.tail-self.head)
-    #     return (self.capacity - (self.head-self.tail))
 
     def get(self):
-        if self.size == 0:
+        if len(self.data) == 0:
             print("Queue is empty")
             return None
         else:
             print(self.data)
             return self.data
-            # curr = self.head
-
-            # for curr in range(self.size):
-            #     print("Value", self.data[curr])
-            #     curr = (curr + 1) % self.capacity
-            #     print(curr)
-            # return self.data[curr]
 
 
 buffer = RingBuffer(3)
@@ -66,10 +34,12 @@ buffer.append('c')
 # print("max")
 buffer.get()
 buffer.append('d')
+buffer.get()
 buffer.append('e')
 # print("replace two")
-# buffer.get()
+buffer.get()
 buffer.append("f")
+buffer.get()
 buffer.append("g")
 # print("after max")
-# buffer.get()
+buffer.get()
