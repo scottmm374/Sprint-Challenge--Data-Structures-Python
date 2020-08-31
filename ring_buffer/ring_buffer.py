8eca3e4e0d25ff40420ef3d9d0b98c73d047aaec
Michelle Scott <scottmm374@gmail.com>
2020-08-30 21:10:30 -0400

    faster time in names

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,13 +1,24 @@
 class RingBuffer:
-    def __init__(self, capacity, next=None):
-        self.capacity = 5
-        self.data = []
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.data = [None] * capacity
         self.tail = -1
         self.head = 0
         self.size = 0
 
     def append(self, item):
-        pass
+        if self.size < self.capacity:
+            item = self.data[self.head]
+            print(item)
+            self.head = (self.head + 1) % self.capacity
+
+            return item
+
+        else:
+
+            self.tail = (self.tail + 1) % self.capacity
+            self.data[self.tail] = item
+            self.size = self.size + 1
 
     def get(self):
         if self.size == 0:
@@ -18,3 +29,12 @@ class RingBuffer:
 
             for curr in range(self.size):
                 curr = (curr + 1) % self.capacity
+
+
+buffer = RingBuffer(5)
+buffer.get()
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+buffer.get()
